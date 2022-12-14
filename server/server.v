14eb4d99b9c486717529a70416706a3e9d9832e1
Your Name <kevin.kds80@gmail.com>
2022-12-14 20:33:11 -0300

    add server routes

---
+++ server/server.v
@@ -0,0 +1,37 @@
+module server
+
+import vweb
+import services
+import structs as s
+import requests as r
+import json
+
+struct App {
+    vweb.Context
+}
+
+pub fn new_shortener() &App {
+    mut app := &App{}
+    return app
+}
+
+['/:link']
+pub fn (mut app App) get_link_url_and_redirect(link string) vweb.Result {
+	return response(mut app, services.get_link_url(link))
+}
+
+['/create'; post]
+pub fn (mut app App) create_link() !vweb.Result {
+	request_body := json.decode(r.CreateLinkRequest, app.req.data)!
+	println(request_body)
+	return response(mut app, services.create_link(request_body))
+}
+
+pub fn response(mut app App, obj s.ResponseObject<s.Response>) vweb.Result {
+	app.set_status(obj.code, 'string que escrevi')
+	return match obj.format {
+		.html { app.html('$obj.body') } 
+		.json { app.json(obj.body) }
+		.text { app.text('$obj.body') }
+	}
+}
